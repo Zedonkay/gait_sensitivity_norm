2b746f044535f91a80f7f74d637413d771266bba
Zedonkay <144646939+Zedonkay@users.noreply.github.com>
2024-07-12 16:26:44 -0400

    calculated entropy

--- histogram.py
+++ histogram.py
@@ -1,22 +1,15 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import scipy.stats as stats
 
-def hist(df):
-    hist = []
-    for data in df:
-        if data[2]==0:
-            continue
-        for i in range(0,int(data[2]*1000)):
-            hist.append([data[0],data[1]])
-    hist = np.array(hist)
-    return hist
+
 
 def main():
     df = pd.read_csv('vertices.csv')
-    histogram = hist(df.values)
-    df = pd.DataFrame(histogram, columns=['x','y'])
-    df.to_csv('histogram.csv', index=False)
+    zs = df['z'].values
+    zs = zs[~pd.isnull(zs)]
+    zs = zs[zs != 0]
+    print(stats.entropy(zs))
+    #7.068082566362332
 
 if __name__ == "__main__":
     main()
