8750aa027e64f64d0c502ed7871d8606f1ba1351
Zedonkay <144646939+Zedonkay@users.noreply.github.com>
2024-09-02 03:34:21 -0400

    real final commit

--- histogram.py
+++ histogram.py
@@ -1,33 +1,18 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import binned_statistic_2d, entropy
 
 def main():
     # Read the data from the CSV file
-    df = pd.read_csv('/Users/ishayu/Documents/GitHub/gait_sensitivity_norm/sim_terrain/vertices.csv')
-    
-    # Extract the 'z' column values from the DataFrame
-    zs = df['z'].values
-    
-    # Remove any NaN values from the 'zs' array
-    zs = zs[~pd.isnull(zs)]
-    
-    # Remove any zero values from the 'zs' array
-    zs = zs[zs != 0]
-    
-    # Calculate the histogram of the 'zs' array to get the probability distribution
-    hist, bin_edges = np.histogram(zs, bins='auto', density=True)
-    
-    # Calculate the entropy of the probability distribution
-    entropy = stats.entropy(hist)
-    
-    # Print the calculated entropy
-    print(entropy)
+    df = pd.read_csv('/Users/ishayu/Documents/GitHub/gait_sensitivity_norm/no_bump/vertices_no_bump.csv')
 
-    
-    #2.662514259040231 for sim terrain
-    #With bump terrain, the entropy is 1.9617905504963942
-    #Without bump terrain, the entropy is 1.8801592023022458
+    zs = df['z'].values  
+    zs = zs[~np.isnan(zs) & (zs != 0)]
+
+    zs = zs/zs.sum()
+    ent = -np.sum(zs*np.log(zs))    
+    print(f"Entropy:{ent}")
+
 
 if __name__ == "__main__":
     main()
